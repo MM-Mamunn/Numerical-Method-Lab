6e8f30ebb48903326cc7bb47803687da69c31ccf
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2025-02-10 13:05:59 +0600

    Update task1-1.02.2025.cpp

--- task1-1.02.2025.cpp
+++ task1-1.02.2025.cpp
@@ -3,17 +3,27 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    string str;
-    getline(cin, str);  // Reading input string
-    stringstream ss(str);
-    int num;
-    vector<int> arr;
+int main()
+{
+    string s;
+    getline(cin,s);
+    vector<int>v;
+    string  temp = "";
+    s += ' ';
+    for( int i = 0 ; i < s.size(); i++)
+    {
+        if(s[i] == ' ')
+        {
+            int x = stoi(temp);
+            v.push_back(x);
+            temp = "";
+            continue;
+        }
+        temp += s[i];
 
-    while (ss >> num) {
-        arr.push_back(num);  // Adding each integer to the vector
     }
+      cout<<v.size()<<endl;
+    for(auto a : v)cout<<a<<' ';cout<<endl;
 
-    cout << arr.size() << endl;  // Printing the size of the vector
     return 0;
 }
