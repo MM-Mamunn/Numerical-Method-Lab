767d7063fe60a8ddf364c74f1312d862a9c20af1
MM-Mamunne <gamesmamun8@gmail.com>
2025-07-06 12:21:50 +0600

    3.12

---
+++ Final/Lab Assignment 3/Lab Assignment 3.12 with custom input.cpp
@@ -0,0 +1,5 @@
+Write a program to solve the following system of linear equations by using Gauss-Seidel method.
+        10x1 +   x2   +  x3     = 12
+         2x1  + 10x2 +  x3    = 13
+         2x1  +   2x2 + 10x3 = 14  
+
