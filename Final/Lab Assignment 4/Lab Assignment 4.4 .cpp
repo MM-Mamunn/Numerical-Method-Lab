ade94bb93b58e2ef096d7e461cfe2897581eca2b
MM-Mamunne <gamesmamun8@gmail.com>
2025-07-12 12:21:33 +0600

    4.4

---
+++ Final/Lab Assignment 4/Lab Assignment 4.4 .cpp
@@ -0,0 +1,43 @@
+/*
+Write a program to solve the following Differential Equation by using Euler’s method.
+        dy / dx = x3 + y,  y (0) = 1. Compute y (0.02) taking h = 0.01.
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+double soln(double x, double y)
+{
+    return (x * x * x ) + y;
+}
+int main()
+{
+  double x, y,h,point;
+  cout<<"Enter x0 :";
+  cin>>x;
+  cout<<"Enter y0 :";
+  cin>>y;
+  cout<<"Enter h:";
+  cin>>h;
+  cout<<"Enter x to compute y(x):";
+  cin>>point;
+  double in = x ,ans = y;
+  do{
+    in += h;
+   ans = ans + (1.00 * h * soln(in,ans));
+  }
+  while(fabs(in - point) > 0.00001);
+  cout<<"y("<<point<<") = "<<ans<<endl;
+}
+
+/*
+Input:
+0 1 .01 .02
+Output:
+y(0.02) = 1.0201
+
+Input:
+0 0 .1 .3
+Output:
+y(0.3) = 0.271
+*/
